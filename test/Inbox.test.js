393d12357722b43a23880ef3d826a36ef325c289
Fiattarone <Fiattarone@me.com>
2022-04-25 16:38:01 -0700

    First commit, files established.

---
+++ test/Inbox.test.js
@@ -0,0 +1,42 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile")
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  // web3.eth.getAccounts()
+  //   .then(fetchedAccounts => {
+  //     console.log(fetchedAccounts);
+  //   });
+
+  //async await refactor
+  accounts = await web3.eth.getAccounts();
+
+  //then deploy contracts using one of the fetched accounts
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi Thereee!"]
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000"
+    })
+});
+
+describe("Inbox", () => {
+  it("Deploys a Contract", () => {
+    // console.log(inbox);
+    assert.ok(inbox.options.address); // checks truthiness
+  })
+
+  it("Has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal()
+  })
+})
